5c9a6861fcef401ee6e24cec8d60d43838a8657d
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-05-24 12:45:47 +0000

    Fixed: eslint

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,12 +3,9 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
-  } else {
-    const count = weakMap.get(endpoint);
-    weakMap.set(endpoint, count + 1);
   }
-  const updatedCount = weakMap.get(endpoint);
-  if (updatedCount >= 5) {
-    throw new Error("Endpoint load is high");
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
   }
 }
