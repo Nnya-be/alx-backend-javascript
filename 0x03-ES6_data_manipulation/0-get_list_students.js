c8d2eddfefadbefd14a7298addf5e26684de5d1c
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-05-24 08:19:16 +0000

    Modified: name in the array

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,7 +2,7 @@ module.exports = function getListStudents() {
   return [
     {
       id: 1,
-      firstName: 'Guilluame',
+      firstName: 'Guillaume',
       location: 'San Francisco',
     },
     {
