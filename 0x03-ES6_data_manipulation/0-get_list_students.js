f21a61e42d56f3183effd430f3a4bf113e840a33
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-05-24 08:17:04 +0000

    Created: list of students functions

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+module.exports = function getListStudents() {
+  return [
+    {
+      id: 1,
+      firstName: 'Guilluame',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+};
